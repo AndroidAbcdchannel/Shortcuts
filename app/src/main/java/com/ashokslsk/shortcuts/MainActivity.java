a07de1e674d8e92818dfac0ec11efdb6a0d34742
ashok.kumar <ashok.kumar@dunstconsulting.in>
2016-11-25 19:26:55 +0530

    Completed final commit.

--- app/src/main/java/com/ashokslsk/shortcuts/MainActivity.java
+++ app/src/main/java/com/ashokslsk/shortcuts/MainActivity.java
@@ -1,15 +1,41 @@
 package com.ashokslsk.shortcuts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mUsername;
+
+    private TextView mUser;
+
+    private static final int age = 86;
+
+    public static final String UserAge = "";
+
+    private static final String TAG = "MainActivity";
+
+    public String getmUsername() {
+        return mUsername;
+    }
+
+    public void setmUsername(String mUsername) {
+        this.mUsername = mUsername;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUser = (TextView) findViewById(R.id.username);
+
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+
+        Log.d(TAG, "onCreate() returned: ");
 
 
         //Shortcut #1
@@ -98,8 +124,62 @@ public class MainActivity extends AppCompatActivity {
         */
 
 
+        //Shortcut #8
+        /*
+        * Surround With
+        *
+        * Mac OS
+        * Opt + Cmd + T
+        *
+        * Linux/Win
+        * Alt + Ctrl + T
+        *
+        */
 
 
+        try {
+            surroundwith();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Shortcut #9
+        /*
+        * Generate Setter/Getters/
+        *
+        * Mac OS
+        * Cmd + N
+        *
+        * Linux/Win
+        * Alt + Ins
+        *
+        */
+
+        //Shortcut #10
+        /*
+        * Search Everywhere
+        *
+        * Mac OS
+        * Shift + Shift
+        *
+        * Linux/Win
+        * Shift + Shift
+        *
+        */
+
+        //Live Templates in Android Studio
+
+        Toast.makeText(MainActivity.this, "Hellow World", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    private void surroundwith() {
+
+        System.out.println("MainActivity.surroundwith");
+        System.out.println("");
+        System.out.println("mUsername = " + mUsername);
+        
 
     }
 
